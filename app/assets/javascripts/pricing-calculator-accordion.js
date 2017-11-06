0beafde0aa6e73e3c5a41fd10ddd22ac7a23ac5d
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2017-11-06 12:28:32 +0000

    Calculating cost of multiple apps

--- app/assets/javascripts/pricing-calculator-accordion.js
+++ app/assets/javascripts/pricing-calculator-accordion.js
@@ -1,9 +1,17 @@
+function add_fields() {
+    var objTo = document.getElementById('room_fileds')
+    var divtest = document.createElement("div");
+    divtest.innerHTML = '<div><table><thead><tr><th class="wide-th small-tr" scope="col"></th><th class="small-tr"scope="col"></th><th class="small-tr"scope="col"></th></tr></thead><tbody><tr class="app-row"><th scope="row"><input name="form-control" type="text" class="form-control" id="form-control" style="width: 360px;" value=""></th><td ><input name="form-control-1-2" type="text" class="form-control form-control-1-2 gb-per-instance" id="form-control-1-2" value=""></td><td ><input name="form-control-1-2" type="text" class="form-control form-control-1-2 instance-count" id="form-control-1-2" value=""></td> </tr></tbody></table></div>';
+
+    objTo.appendChild(divtest)
+}
+
 $(document).ready(function () {
-    $(".memory-usage input").keyup(function (e) {
+    $(".memory-usage").on("keyup", "input", function (e) {
         var total_gb_used = 0.0
         $(".app-row").each(function () {
-            var gb_per_instance = parseFloat($("input.gb-per-instance", this).val())
-            var instance_count = parseInt($("input.instance-count", this).val())
+            var gb_per_instance = parseFloat($("input.gb-per-instance", this).val()) || 0
+            var instance_count = parseInt($("input.instance-count", this).val()) || 0
             total_gb_used += gb_per_instance * instance_count
         })
         console.log("total_gb_used: " + total_gb_used)
@@ -11,7 +19,7 @@ $(document).ready(function () {
         var cost_of_instances = total_gb_used * 18.23773352
         console.log("cost_of_instances: £" + cost_of_instances)
 
-        var number_of_zero_downtime_deploys = parseFloat($("input.zero-downtime-deploy-count").val())
+        var number_of_zero_downtime_deploys = parseFloat($("input.zero-downtime-deploy-count").val()) || 0
         var cost_of_zero_downtime_instances = Math.min(number_of_zero_downtime_deploys * total_gb_used * 0.03747068852, cost_of_instances)
         console.log("cost_of_zero_downtime_instances: £" + cost_of_zero_downtime_instances)
     })
