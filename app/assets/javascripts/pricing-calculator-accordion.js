45a28d519f04df1618cef85af4dcbd5e0ac899af
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2017-11-06 12:14:27 +0000

    Highly-improvised calculation of app charges

--- app/assets/javascripts/pricing-calculator-accordion.js
+++ app/assets/javascripts/pricing-calculator-accordion.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+    $(".memory-usage input").keyup(function (e) {
+        var total_gb_used = 0.0
+        $(".app-row").each(function () {
+            var gb_per_instance = parseFloat($("input.gb-per-instance", this).val())
+            var instance_count = parseInt($("input.instance-count", this).val())
+            total_gb_used += gb_per_instance * instance_count
+        })
+        console.log("total_gb_used: " + total_gb_used)
+
+        var cost_of_instances = total_gb_used * 18.23773352
+        console.log("cost_of_instances: £" + cost_of_instances)
+
+        var number_of_zero_downtime_deploys = parseFloat($("input.zero-downtime-deploy-count").val())
+        var cost_of_zero_downtime_instances = Math.min(number_of_zero_downtime_deploys * total_gb_used * 0.03747068852, cost_of_instances)
+        console.log("cost_of_zero_downtime_instances: £" + cost_of_zero_downtime_instances)
+    })
+
     $("input[type=radio][name=relational]").change(function (e) {
         console.log("relational: £" + parseFloat($("input[type=radio][name=relational]:checked").val()))
     })
