00b64a66016a7d9f712a6e47c3e16ba7941afd64
Caroline Green <caroline.green@digital.cabinet-office.gov.uk>
2017-11-06 14:53:50 +0000

    Add calculator and getter and setter methods for currency
    Add caclulator to add price.  Add method to retrieve current cost from environment table and also method to add cost to table.

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -3,28 +3,44 @@
     var $allInputs = $(this).find('input')
     console.log('allInputs', $allInputs)
     var $inputs = []
-    //var $memoryUsageInputs = $('.table-memory-usage').find('input')
-    //console.log($memoryUsageInputs)
     var $memoryUsageMemory = $("input[name='memory']")
     var $memoryUsageInstances = $("input[name='instances']")
     console.log('memoryUsage', $memoryUsageMemory)
 
     $memoryUsageMemory.blur(function () {
       var $envCost = $('.environment-cost')
-      var $cost = getCurrency($envCost)
-      /*var $cost = $envCost.html()
-      $cost = Number($cost.replace(/[^0-9\.-]+/g,""))
-      $cost = $cost * 100*/
-      console.log('cost', $cost)
-      console.log($envCost)
-      $envCost.html('test')
+      var cost = convertToNumber($envCost)
+      var memoryUsageTotal = calculateMemoryUsage($memoryUsageMemory.val(), $memoryUsageInstances.val())
+      var newCost = cost + memoryUsageTotal
+      var costFormatted = convertToCurrency(newCost)
+      $envCost.html(costFormatted)
     })
 
-    function getCurrency ($costInCurrency) {
+    function addCostToCalculator (currentCost, costAdded) {
+      var totalCost = currentCost + costAdded
+      return totalCost
+    }
+
+    function calculateMemoryUsage ($memoryUsageMemory, $memoryUsageInstances) {
+      var $memoryUsage = $memoryUsageMemory * $memoryUsageInstances
+      var costPerGbHour = 0.009704918033
+      var dollarToPound = 0.78
+      var hoursPerMonth = 730.08
+      var $memoryUsageCost = $memoryUsage * costPerGbHour * dollarToPound * hoursPerMonth
+      return $memoryUsageCost
+    }
+
+    function convertToNumber ($costInCurrency) {
       var $cost = $costInCurrency.html()
       $cost = Number($cost.replace(/[^0-9\.-]+/g,""))
       $cost = $cost * 100
       return $cost
+    }
+
+    function convertToCurrency ($costAsNumber) {
+      $costAsNumber = parseFloat($costAsNumber/100).toFixed(2);
+      $costAsNumber = '£' + $costAsNumber
+      return $costAsNumber
     }
 
     $memoryUsageInstances.blur(function () {
