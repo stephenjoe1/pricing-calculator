beb0c11b8f8a7c540e2cbef4a32851c4f25768fb
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2017-11-06 11:54:18 +0000

    Calculating cost of Elasticsearch and Redis

--- app/assets/javascripts/pricing-calculator-accordion.js
+++ app/assets/javascripts/pricing-calculator-accordion.js
@@ -6,4 +6,12 @@ $(document).ready(function () {
     $("input[type=radio][name=mongo]").change(function (e) {
         console.log("mongo: £" + parseFloat($("input[type=radio][name=mongo]:checked").val()))
     })
+
+    $("input[type=radio][name=elasticsearch]").change(function (e) {
+        console.log("elasticsearch: £" + parseFloat($("input[type=radio][name=elasticsearch]:checked").val()))
+    })
+
+    $("input[type=radio][name=redis]").change(function (e) {
+        console.log("redis: £" + parseFloat($("input[type=radio][name=redis]:checked").val()))
+    })
 })
